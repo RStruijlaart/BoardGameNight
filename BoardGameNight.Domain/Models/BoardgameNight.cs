747fc7db43290024a8656948c1fab90c53cba217
RStruijlaart <79460280+RStruijlaart@users.noreply.github.com>
2024-10-16 11:23:08 +0200

    quickfix

--- BoardGameNight.Domain/Models/BoardgameNight.cs
+++ BoardGameNight.Domain/Models/BoardgameNight.cs
@@ -1,4 +1,10 @@
-﻿namespace BoardGameNight.Domain.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameNight.Domain.Models
 {
     public class BoardgameNight
     {
